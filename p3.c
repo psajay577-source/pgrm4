6357fa8a41a4a69ece1648b778a01044b3324148
psajay577-source <psajay577@gmail.com>
2025-11-14 15:05:31 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("enter the x value");
+    scanf("%d",x);
+    printf("enter the y value ");
+    scanf("%d",y);
+    if(x>y)
+    {
+        printf("x is grater");
+    }
+    else
+    {
+        printf("y is grater");
+    }
+}
